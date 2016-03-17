d855480778e219ebe2a9aec78bbc4bc265b5be1b
Constantin Slednev <c.slednev@gmail.com>
2016-03-17 19:50:23 +0300

    start using fabric

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import local
+
+
+def deploy():
+    local('pip freeze > requirements.txt')
+    local('git add .')
+    print("enter your git commit comment: ")
+    comment = raw_input()
+    local('git commit -m "%s"' % comment)
+    local('git push -u origin master')
+    local('heroku maintenance:on')
+    local('git push heroku master')
+    local('heroku run python backend/manage.py migrate')
+    local('heroku maintenance:off')
