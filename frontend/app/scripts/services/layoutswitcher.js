3578de3112d05f0e818408b29696b39ed0251cf1
Ilya Gorbachevsky <igorbachevsky@gmail.com>
2016-03-18 08:37:51 +0300

    Menubar toggle, directives, small adjustments

--- frontend/app/scripts/services/layoutswitcher.js
+++ frontend/app/scripts/services/layoutswitcher.js
@@ -8,18 +8,33 @@
  * Factory in the ClikhomeApp.
  */
 angular.module('ClikhomeApp')
-  .factory('layoutSwitcher', ['$rootScope', 'LAYOUT_TYPES', function ( $rootScope, LAYOUT_TYPES ) {
-    var EVENT_NAME = 'layout-switcher:layout';
+  .factory('layoutSwitcher', ['$rootScope', 'LAYOUT_TYPES', function ($rootScope, LAYOUT_TYPES) {
+    var LAYOUT_EVENT = 'layout-switcher:layout';
+    var MENU_EVENT = 'layout-switcher:toggleMenu';
+    var MENU_MOBILE_EVENT = 'layout-switcher:toggleMenuMobile';
 
-    return {
+    var service = {
       LAYOUT_TYPES: LAYOUT_TYPES,
-      layout: function ( type ) {
-        if ( angular.isUndefined(type) ) {
+      menuExpanded: false,
+      menuMobileExpanded: false,
+      layout: function (type) {
+        if (angular.isUndefined(type)) {
           console.warn('Invalid layout type undefined');
           return false;
         }
-        this.selected = type;
-        $rootScope.$emit(EVENT_NAME, type);
+        service.selected = type;
+        $rootScope.$emit(LAYOUT_EVENT, type);
+      },
+      toggleMenu: function () {
+        service.menuExpanded = !service.menuExpanded;
+        $rootScope.$emit(MENU_EVENT, service.menuExpanded);
+      },
+      toggleMenuMobile: function () {
+        service.menuMobileExpanded = !service.menuMobileExpanded;
+        $rootScope.$emit(MENU_MOBILE_EVENT, service.menuExpanded);
       }
     };
+    service.toggleMenu();
+
+    return service;
   }]);
