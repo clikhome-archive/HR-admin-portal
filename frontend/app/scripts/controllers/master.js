69155bd586e71b1c1080eab16ea860850acae9b9
Ilya Gorbachevsky <igorbachevsky@gmail.com>
2016-03-24 16:03:25 +0300

    Improve layout switching on auth, /forgot-password markup, #44

--- frontend/app/scripts/controllers/master.js
+++ frontend/app/scripts/controllers/master.js
@@ -8,11 +8,16 @@ angular.module('ClikhomeApp')
     // Assume user is not logged in until we hear otherwise
     $scope.authenticated = false;
     // Wait for the status of authentication, set scope var to true if it resolves
-    djangoAuth.authenticationStatus(true).then(function(response){
-      $rootScope.$broadcast("djangoAuth.logged_in", {user: response});
-    });
+    djangoAuth.authenticationStatus(true).then(
+      function (response) {
+        $rootScope.$broadcast("djangoAuth.logged_in", { user: response });
+        layoutSwitcher.layout(layoutSwitcher.LAYOUT_TYPES.MENU_EXPANDED);
+      },
+      function () {
+        layoutSwitcher.layout(layoutSwitcher.LAYOUT_TYPES.LOGIN);
+      });
     // Wait and respond to the logout event.
-    $scope.$on('djangoAuth.logged_out', function() {
+    $scope.$on('djangoAuth.logged_out', function () {
       $scope.authenticated = false;
       layoutSwitcher.layout(layoutSwitcher.LAYOUT_TYPES.LOGIN);
       $scope.email = null;
@@ -22,9 +27,8 @@ angular.module('ClikhomeApp')
       $scope.department_name = null;
     });
     // Wait and respond to the log in event.
-    $scope.$on('djangoAuth.logged_in', function(event, response) {
+    $scope.$on('djangoAuth.logged_in', function (event, response) {
       $scope.authenticated = true;
-      layoutSwitcher.layout(layoutSwitcher.LAYOUT_TYPES.MENU_EXPANDED);
       $scope.email = response.user.email;
       $scope.first_name = response.user.first_name;
       $scope.last_name = response.user.last_name;
@@ -32,11 +36,11 @@ angular.module('ClikhomeApp')
         $scope.company_name = response.user.company.name;
       }
       if (response.user.department) {
-        $scope.department_name = response.user.department[response.user.department.length-1].name;
+        $scope.department_name = response.user.department[response.user.department.length - 1].name;
       }
 
     });
-    $scope.$back = function() {
+    $scope.$back = function () {
       window.history.back();
     };
   }]);
