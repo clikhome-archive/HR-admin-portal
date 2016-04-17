1b7241e91e36b83f6f8ae0fb117cd9e7b8e428ec
Constantin Slednev <c.slednev@gmail.com>
2016-04-18 02:14:07 +0300

    fix bug with mailing task arguments

--- backend/employee/tasks.py
+++ backend/employee/tasks.py
@@ -3,7 +3,7 @@ from celery_app import app
 
 
 @app.task(bind=True, default_retry_delay=300, max_retries=5)
-def send_managers_email(subject, text_content, html_content, from_email, mamagers_emails):
+def send_managers_email(self, subject, text_content, html_content, from_email, mamagers_emails):
     msg = EmailMultiAlternatives(subject, text_content, from_email, mamagers_emails)
     msg.attach_alternative(html_content, "text/html")
     msg.send()
